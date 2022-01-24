bc2c77ca400d9ccae3d52a04650d89a47648e930
TheAshenWolf <jirirkas.skola@gmail.com>
2022-01-24 19:38:02 +0100

    Added mirror and started implementing multiplayer

---
+++ Runtime/Multiplayer/Core/Scripts/NetworkManagerVR.cs
@@ -0,0 +1,15 @@
+﻿#if MIRROR
+using Mirror;
+
+namespace TAW_VR.Runtime.Multiplayer.Core.Scripts
+{
+    public class NetworkManagerVR : NetworkManager
+    {
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+        }
+    }
+}
+
+#endif
