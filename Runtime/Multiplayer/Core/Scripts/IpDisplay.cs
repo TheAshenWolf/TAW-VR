975b131716e89ee4e3f61615fa2bafc51a738414
TheAshenWolf <jirirkas.skola@gmail.com>
2022-01-29 20:05:38 +0100

    Added IP display

---
+++ Runtime/Multiplayer/Core/Scripts/IpDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using TMPro;
+using UnityEngine;
+
+namespace TAW_VR.Runtime.Multiplayer.Core.Scripts
+{
+  public class IpDisplay : MonoBehaviour
+  {
+    public TextMeshPro ipField;
+
+    private void Start()
+    {
+      IPHostEntry host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+      foreach (IPAddress ip in host.AddressList)
+      {
+        if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+        {
+          ipField.text = ip.ToString();
+        }
+      }
+    }
+  }
+}
